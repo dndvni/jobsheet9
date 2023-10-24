6933498a9fa2cdfd48eb2a4a8dba064ab2ad456b
vani <vani.suw@gmail.com>
2023-10-24 15:53:58 +0700

    jawaban

--- ArrayNilai02.java
+++ ArrayNilai02.java
@@ -6,14 +6,18 @@ public class ArrayNilai02{
 
         int[] nilaiAkhir = new int[10];
 
-        for (int i= 0; i < 10; i++) {
-            System.out.print("Masukkan nilai akhir ke-"+i+" : ");
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            System.out.print("Masukkan nilai akhir ke-"+i+":");
             nilaiAkhir[i] = sc.nextInt();
         }
         
+        String status;
         for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
+            if (nilaiAkhir[i] > 70) {
+                status = "lulus!";
+            } else 
+                status = "tidak lulus!";
+            System.out.println("Mahasiswa ke-"+i+" "+status);
         }
-
     }
 }
