c76883ae8613c855fa12a7f1e449e79591cfa041
vani <vani.suw@gmail.com>
2023-10-24 07:36:23 +0700

    first commit

---
+++ ArrayRataNilai.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ArrayRataNilai{
+    public static void main(String[] args){
+        Scanner input02 = new Scanner(System.in);
+
+        int[] nilaiMhs = new int[10];
+        double total = 0;
+        double rata2;
+
+        for (int i = 0; i < nilaiMhs.length; i++){
+            System.out.print("Masukkan nilai Mahasiswa ke-"+(i+1)+" : ");
+            nilaiMhs[i] = input02.nextInt();
+
+            for (i = 0; i < nilaiMhs.length; i++) {
+                total += nilaiMhs[i];
+                
+            }
+            
+
+                rata2 = total/nilaiMhs.length;
+                System.out.println("Rata-rata nilai = "+rata2);
+
+                input02.close();
+            }
+        }
+    }
